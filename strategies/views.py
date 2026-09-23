e476bc242bbe9dd9393174803037754e33731ea8
waleed smadi <waleed0smadi@outlook.com>
2026-09-24 02:12:01 +0300

    Refactor(strategies): make the function  in views.py show the strategies in

--- strategies/views.py
+++ strategies/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Strategy
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
 
 
+@login_required(login_url='login:login_view_url')
 def show_strategies(request):
-    return render(request, 'strategies/strategies.html')
+    strats = Strategy.objects.filter(user=request.user)
+    return render(request, 'strategies/strategies.html', {'strats': strats})
