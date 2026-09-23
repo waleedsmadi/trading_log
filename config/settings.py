8499690a359886305246851425795fc841b6ac42
waleed smadi <waleed0smadi@outlook.com>
2026-09-23 22:49:10 +0300

    Chore: Create media settings in  and installing  library

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
